7a5216f87c4ca43c2e869f2cc4cafefb46b1a62d
Rory Stokes <rmstokes@agiledigital.com.au>
2018-01-31 14:43:58 +1100

    Create config directory if it doesn't exist

--- app/run.sh
+++ app/run.sh
@@ -3,6 +3,7 @@
 cd /home/runner/artifacts
 
 #Generate and inject config.json into dist
+mkdir -p .next/src/dist/config
 java -cp /home/runner/tools/confy-assembly-3.0.jar confy.app.ConfigurationApp -i conf/combined.conf > .next/src/dist/config/config.json
 
 ./node_modules/.bin/next start
