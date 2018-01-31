c10842568dead73f5a8b44e1c7fbf3ac0994af7e
Rory Stokes <rmstokes@agiledigital.com.au>
2018-01-31 15:11:48 +1100

    fix config directory location in dist

--- app/run.sh
+++ app/run.sh
@@ -4,6 +4,6 @@ cd /home/runner/artifacts
 
 #Generate and inject config.json into dist
 mkdir -p .next/src/dist/config
-java -cp /home/runner/tools/confy-assembly-3.0.jar confy.app.ConfigurationApp -i conf/combined.conf > .next/src/dist/config/config.json
+java -cp /home/runner/tools/confy-assembly-3.0.jar confy.app.ConfigurationApp -i conf/combined.conf > .next/dist/config/config.json
 
 ./node_modules/.bin/next start
